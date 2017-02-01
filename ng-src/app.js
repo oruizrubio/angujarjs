cc6f7e044c7b5b4eee99c12152a7d8dc3bd0416d
oruizrubio <oruizrubio@gmail.com>
2017-02-01 22:15:08 +0100

    directivas 1
    adaptación al video de las directivas 1 del curso

--- ng-src/app.js
+++ ng-src/app.js
@@ -1,6 +1,18 @@
 angular.module("CustomDirective",[])
+.directive('backImg',function(){
+	return function(scope,element,attrs){
+	
+		attrs.$observe('backImg',function(value){
+			element.css({
+				"background": "url("+value+")",
+				"background-size": "cover",
+				"background-position": "center"
+			});
+		});
+	}
+})
 .controller("AppCtrl", function($scope,$http){
-	$http.get("https://api.github.com/users/codigofacilito/repos")
+	$http.get("https://api.github.com/users/oruizrubio/repos")
 		.success(function(data){
 			$scope.repos = data;
 		})
