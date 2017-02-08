fbabe147b363b10188217bac54f417012fb06cc1
oruizrubio <oruizrubio@gmail.com>
2017-02-08 20:56:21 +0100

    video 22 del curso
    video 22 del curso, 2ª parte de la práctica final, se ven los resources para atacar a un api-rest

--- resource/js/controllers.js
+++ resource/js/controllers.js
@@ -1,4 +1,10 @@
 angular.module("FinalApp")
-.controller("MainController",function($scope){
-
+.controller("MainController",function($scope, $resource){
+	// acceder a un API REST (en este caso uno de pruebas)
+	Post = $resource("http://jsonplaceholder.typicode.com/posts/:id",{id: "@id"});
+	User = $resource("http://jsonplaceholder.typicode.com/users/:id",{id: "@id"});
+	$scope.posts = Post.query();
+	$scope.users = User.query();
+	// query() --> GET /posts -> array de 
+	
 });
