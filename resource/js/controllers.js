e64c6987cbd1bad68005254577327b59bbbddbc3
oruizrubio <oruizrubio@gmail.com>
2017-02-08 21:22:18 +0100

    video 23, 3ª parte práctica final
    consultar y borrar un registro de un API

--- resource/js/controllers.js
+++ resource/js/controllers.js
@@ -5,6 +5,24 @@ angular.module("FinalApp")
 	User = $resource("http://jsonplaceholder.typicode.com/users/:id",{id: "@id"});
 	$scope.posts = Post.query();
 	$scope.users = User.query();
-	// query() --> GET /posts -> array de 
+	// query() --> GET /posts -> devuelve array
 	
+	$scope.removePost = function(post) {
+		Post.delete({id: post.id}, function(data){
+			console.log(data);
+		});
+		// en un API REST normal, esto es lo que habría que hacer.  Placeholder aunque devuelve true, realmente no borra el recurso y por eso lo vamos a simular
+		//$scope.posts = Post.query();		
+		// con esta forma de hacerlo, no habría que volver a reconsultar el API, lo cual es más eficiente
+		$scope.posts = $scope.posts.filter(function(element){
+			return element.id !== post.id;
+		});
+		
+	}
+	
+})
+.controller("PostController",function($scope,$resource,$routeParams){
+	Post = $resource("http://jsonplaceholder.typicode.com/posts/:id",{id: "@id"});
+	$scope.post = Post.get({id: $routeParams.id});
+	// get() --> GET/post -> devuelve objeto
 });
